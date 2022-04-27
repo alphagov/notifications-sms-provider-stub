6119c55a100e4f79fa4098cbff61588cdb748122
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2022-04-27 18:05:59 +0100

    Remove support for Reach provider
    We're no longer going to integrate with this provider.

--- server.go
+++ server.go
@@ -8,7 +8,6 @@ import (
 func main() {
 	http.HandleFunc("/mmg", MmgEndpoint)
 	http.HandleFunc("/firetext", FiretextEndpoint)
-	http.HandleFunc("/reach", ReachEndpoint)
 	port := getenv("PORT", "6300")
 	log.Printf("Listening on port %s...\n", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
